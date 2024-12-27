035f8501dd437f46ca434920be247e433655bcb5
Prachi3899 <116147549+Prachi3899@users.noreply.github.com>
2024-12-27 19:31:01 +0530

    login-signin page done

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+  border-bottom: 1px solid ${({ theme }) => theme.text_secondary + 20};
+`;
+
+const NavContainer = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 24px;
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <NavContainer></NavContainer>
+    </Nav>
+  )
+}
+
+export default Navbar
